924fe837888c67b3031e4d846a1f49de46b440d4
Alex Sea <seagullalex81@gmail.com>
2024-08-16 01:04:19 +0300

    add mobile project section

--- src/js/project-swiper.js
+++ src/js/project-swiper.js
@@ -1,2 +1,2 @@
-import { Swiper, Navigation, Pagination } from './libs';
-console.log('Hello-world project');
+// import { Swiper, Navigation, Pagination } from './libs';
+// console.log('Hello-world project');
