27f3b1f49c3d2ef7b6ed795e581814bdea0a667d
Volodymyr Luniv <clinic118811@gmail.com>
2024-08-17 15:55:07 +0300

    create markup about and add accordion

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,2 +1,23 @@
 import { Swiper, Navigation, Pagination, Accordion } from './libs';
-console.log('Hello-world-about me');
+new Accordion('.accordion-container', {
+  ariaEnabled: true,
+  duration: 400,
+  collapse: true,
+  elementClass: 'acc',
+  triggerClass: 'ac-trigger',
+  panelClass: 'ac-panel',
+  activeClass: 'is-active',
+  openOnInit: [0],
+  beforeOpen: currElement => {
+    console.log('Before opening:', currElement);
+  },
+  onOpen: currElement => {
+    console.log('Opened:', currElement);
+  },
+  beforeClose: currElement => {
+    console.log('Before closing:', currElement);
+  },
+  onClose: currElement => {
+    console.log('Closed:', currElement);
+  },
+});
