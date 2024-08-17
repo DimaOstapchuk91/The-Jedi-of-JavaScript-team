9a7511e2c5988940cf81cc254712cf11ab3dfc8b
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-08-17 14:31:06 +0300

    Update faq-accordion.js

--- src/js/faq-accordion.js
+++ src/js/faq-accordion.js
@@ -7,14 +7,12 @@ new Accordion(document.querySelector('.faq-accordion-container'), {
   panelClass: 'faq-panel',
   triggerClass: 'faq-trigger',
   onOpen: function (currentElement) {
-    console.log(currentElement);
     const arrow = currentElement.querySelector('.faq-arrow');
     if (arrow) {
       arrow.style.transform = 'rotate(180deg)';
     }
   },
   onClose: currentElement => {
-    console.log(currentElement);
     const arrow = currentElement.querySelector('.faq-arrow');
     if (arrow) {
       arrow.style.transform = 'rotate(0deg)';
