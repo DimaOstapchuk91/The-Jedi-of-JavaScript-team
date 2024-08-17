41d7d80549c345b701ba01e6d8dc69c55d115145
Alex Sea <seagullalex81@gmail.com>
2024-08-17 23:42:20 +0300

    add all project sections

--- src/js/libs.js
+++ src/js/libs.js
@@ -3,14 +3,15 @@ import axios from 'axios';
 
 // Swiper
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, Keyboard } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/keyboard';
 
 //Accordion
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-export { Swiper, Navigation, Pagination, axios, Accordion };
+export { Swiper, Navigation, Pagination, Keyboard, axios, Accordion };
